d3588a532c767a60b3b2e32142afb1c3cf02bf4f
ProjetoMensal <fvcominetti@gmail.com>
2024-11-05 19:53:56 -0300

    feat: Menu atualizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './home/home/home.component';
 import { GeneralComponent } from './home/general/general.component';
 import { ClientEditComponent } from './home/client-edit/client-edit.component';
 import { AuthGuard } from './auth/auth.guard';
+import { GerenciarComponent } from './home/produtos/gerenciar/gerenciar.component';
+import { FornecedoresComponent } from './home/fornecedores/fornecedores.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -14,6 +16,8 @@ export const routes: Routes = [
         children: [
             { path: '', component: GeneralComponent, },
             { path: 'cliente-editar', component: ClientEditComponent, runGuardsAndResolvers: 'always' },
+            { path: 'produtos', component: GerenciarComponent, runGuardsAndResolvers: 'always' },
+            { path: 'fornecedores', component: FornecedoresComponent, runGuardsAndResolvers: 'always' },
         ]
     },
     { path: '**', redirectTo: 'login' },
