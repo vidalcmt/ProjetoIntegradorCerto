17371f0dee13295cc33b0fbc83655663acc8ca62
kolpzs <zjoaopedrosantosz@gmail.com>
2024-11-05 20:01:55 -0300

    Produto Service

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Produto } from '../models/produto.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
+  private apiURL = 'http://localhost:8080/produto';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findById(id: number): Observable<Produto> {
+    return this.http.get<Produto>(`${this.apiURL}/findById?id=${id}`);
+  }
+
+  findAll(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.apiURL}/findAll`);
+  }
+
+  findByNome(nome: string): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.apiURL}/findByNome?nome=${nome}`);
+  }
+
+  findByFornecedorId(fornecedorId: number): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.apiURL}/findAllByFornecedorId?fornecedorId=${fornecedorId}`);
+  }
+
+  findByMarca(marca: string): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.apiURL}/findByMarca?marca=${marca}`);
+  }
+
+  findByEstoque(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.apiURL}/findByEstoque`);
+  }
+
+  relatorioEstoque(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiURL}/relatorioEstoque`);
+  }
 }
